406e2e254e87aed467805896457d051a0b31d758
alanbaldi <alanortizbaldizon@hotmail.com>
2021-01-07 16:02:52 -0500

    se agrego el middleware de permisos

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-trait Permit {
+trait Admissibleness {
 
     public function permissions(){
         try{
